9095264293c77711ba1ef33dabfbd9102e4d4414
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2025-12-03 00:07:17 +0800

    uses next_public for live token for client side or pricing page.

--- app/shop/pricing/page.tsx
+++ app/shop/pricing/page.tsx
@@ -49,8 +49,8 @@ const PremiumWelcome = () => {
     if (env.env === 'production') {
       initializePaddle({
         environment: "production", // Use "sandbox" for testing
-        token: 'live_75e8b184e28463f4153fc4d2388',
-        // token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN_LIVE!,
+        // token: 'live_75e8b184e28463f4153fc4d2388',
+        token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN_LIVE!,
 
       }).then((p) => setPaddle(p));
     }
